33adcefa4e25e77a14bcd07c76211e041344954c
Irvin Campos <irvincampos76@gmail.com>
2021-06-23 21:16:26 -0700

    secret

---
+++ dojo_survey_assignment/app_one/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse, redirect
+def index(request):
+    return render(request, "index.html")
+
+def result(request):
+    if request.method == "GET":
+        return redirect("/")
+    if request.method == "POST":
+        context = {
+            'your_name': request.POST['your_name'],
+            'dojo_location': request.POST['dojo_location'],
+            'favorite_language': request.POST['favorite_language'],
+            'comment': request.POST['comment'],
+        }
+        return render(request, "result.html", context)
