e22132f322f454a272b84cf361a3c40cb5abf90e
Irvin Campos <irvincampos76@gmail.com>
2021-06-23 21:38:25 -0700

    secrets

--- dojo_survey_assignment/dojo_survey_assignment/settings.py
+++ dojo_survey_assignment/dojo_survey_assignment/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from . import secrets
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bez7%6u%95fu=7h@5i$)knr2)6o#y(b6v9#)0wwyy#a4g%61wl'
+SECRET_KEY = secrets.SECRET KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
